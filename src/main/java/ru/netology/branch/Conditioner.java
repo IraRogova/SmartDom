707a471313f1210f323ee723ba8fccc2364ecb37
Irina Rogova <buyersbirds@gmail.com>
2020-11-24 11:00:58 +0600

    Initinal commit

---
+++ src/main/java/ru/netology/branch/Conditioner.java
@@ -0,0 +1,4 @@
+package ru.netology.branch;
+
+public class Conditioner {
+}
---
+++ src/test/java/ru/netology/branch/ConditionerTest.java
@@ -0,0 +1,7 @@
+package ru.netology.branch;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+
+}
